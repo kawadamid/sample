3d5d19c4dd8064b992fedea257b903093385e086
kawadamid <152497449+kawadamid@users.noreply.github.com>
2024-05-31 22:00:14 +0900

    20240531

---
+++ src/jp/co/diworks/action/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package jp.co.diworks.action;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("Helloword");
+		int total=gokei();
+		System.out.println(total);
+
+	}
+	public static int gokei(){
+		return 1+1;
+	}
+
+}
